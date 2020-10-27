d2cf19dde924f64761618b5a8651f612611b9d26
nupaT <trash_jack@mail.ru>
2020-10-27 12:48:42 +0300

    add: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
 module.exports = function toReadable (number) {
   
-  function toReadable (number) {
     let result;
-    let val = String(number);
-    let units = String(val % 10);
-    let deci = Math.trunc(val/10);
-      alert(val<100);
-    alert(deci);
+    // let units = (number % 10);
+    let decimal = (number%100);
+    let hundreds = (Math.trunc(number/100));
   
-    if(val < 10){
-      return reply(val);
-  
-    } else if(10 <= val < 20){
-      return replyDec(val - 10);
-  
-    }
-  
+    if(number == 0){
+      result = reply(number);
+    }else if(0 < number && number < 20){
+          result = reply(number);
+    } else if(20 <= number && number < 100){
+      if(decimal%10 == 0){
+        result = decim(Math.trunc(decimal/10));
+      } else {
+        result = decim(Math.trunc(decimal/10)) + ' ' + reply(decimal%10);
+        }
+      } else if(100 <= number && number < 1000){
+        if(number%100 == 0){
+          result = reply(hundreds) + ' hundred';
+        } else if (0 < number%100 &&  number%100 < 20){
+          result = reply(hundreds) + ' hundred' + ' ' + reply(number%100);
+          } else if(decimal%10 == 0){
+            result = reply(hundreds) + ' hundred' + ' ' + decim(Math.trunc(decimal/10));
+            }else{
+             result = reply(hundreds) + ' hundred' + ' ' + decim(Math.trunc(decimal/10)) + ' ' + reply(decimal%10);
+             }
+       }
+    return result;
   }
   
   function reply(x){
-      let arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+      let arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+      'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
       return arr[x];
     };
   
-  function replyDec(x){
-    let arr = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  function decim(x){
+    let arr = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     return arr[x];
   };
-  
-  function decim(x){
-    let arr = ['0', '1', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    return decim[x];
-  };
-  
-  toReadable(25);
-   //will return 'nine hundred ninety seven'   //will return 'nine hundred ninety seven'
-}
 
+  
